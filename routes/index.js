fba445404d8f5b19508e5cf60c691c26c999d998
Aaron H. English <Aaron.H.English@gmail.com>
2017-05-25 22:34:48 -0500

    Adds better placeholder text for our project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var recipes = [
+  {
+    id: 1,
+    title: "Meat Balzz"
+  },
+  {
+    id: 2,
+    title: "Organic Boiled Water"
+  },
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { recipes: recipes });
 });
 
 module.exports = router;
